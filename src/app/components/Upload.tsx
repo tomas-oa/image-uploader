9b797da9383f57093e2657f47e14a247e8a7bf19
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-06-20 21:41:20 -0400

    dnd functionallities added

--- src/app/components/Upload.tsx
+++ src/app/components/Upload.tsx
@@ -1,13 +1,70 @@
+"use client"
+import { useState } from 'react'
 import DragAndDropBox from './DragAndDropBox'
 
+function isImage(file: File): boolean {
+  const allowedExtensions = ["jpg", "jpeg", "png", "gif"];
+  const fileName = file.name;
+  const fileExtension = fileName.split(".").pop()?.toLowerCase();
+  return !!fileExtension && allowedExtensions.includes(fileExtension);
+}
+
+
 export default function Upload () {
+  const [file, setFile] = useState<File | null>(null)
+
+  function handleUpload (e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0]
+
+    if (!file) {
+      alert('No file selected')
+      setFile(null)
+      return
+    } else if (!isImage(file)) {
+      alert('File is not an image')
+      setFile(null)
+      return
+    } else {
+      setFile(file)
+    }
+  }
+
+  function handleDrag (e: React.DragEvent<HTMLLabelElement>) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+
+  function handleDrop (e: React.DragEvent<HTMLLabelElement>) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    const file = e.dataTransfer.files?.[0]
+    
+    if (!file) {
+      alert('No file selected')
+      setFile(null)
+      return
+    } else if (!isImage(file)) {
+      alert('File is not an image')
+      setFile(null)
+      return
+    } else {
+      setFile(file)
+    }
+  }
+
   return (
-    <div className="min-w-[402px] min-h-[469px] flex flex-col items-center p-10 rounded-2xl shadow-lg">
+    <form className="min-w-[402px] min-h-[469px] flex flex-col items-center p-10 rounded-2xl shadow-lg">
       <h1 className="text-xl mb-5">Upload your image</h1>
       <small className="text-[#808080] text-xs font-medium"> File should be Jpeg, Png...</small>
-      <DragAndDropBox />
+      <label htmlFor="file_upload" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}>
+        <DragAndDropBox />
+      </label>
       <small className="text-[#808080] my-6">Or</small>
-      <button className="rounded-lg bg-[#09f] text-white py-2 px-3 text-sm">Choose a file</button>
-    </div>
+      <label className="rounded-lg bg-[#09f] text-white py-2 px-3 text-sm">
+        Choose file
+        <input onChange={handleUpload} id="file_upload" type="file" className="hidden" accept='image/*'/>
+      </label>
+    </form>
   )
 }
