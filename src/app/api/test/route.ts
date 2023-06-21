b2769b2f43bab752353de9724abc9766805e7a05
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-06-21 19:38:17 -0400

    test api route removed

--- src/app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
-
-export async function GET(req: NextApiRequest) {
-    const url = new URL(req.url || "asdf", `http://${req.headers.host}`)
-    const name = url.searchParams.get('name')
-    
-    return NextResponse.json({ message: name }, { status: 200 })
-}
