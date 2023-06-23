6670247c0c9ab1de836764cc9c276fdc4e855703
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-06-23 13:57:00 -0400

    copy button styles fixed

--- src/app/components/CopyToClipboard.tsx
+++ src/app/components/CopyToClipboard.tsx
@@ -26,10 +26,10 @@ export default function CopyToClipboard ({ url }: Props) {
     
   return (
     <div className="relative">
-      <div className="block min-w-full p-2 py-4 text-sm text-[#4F4F4F] border border-[#E0E0E0] rounded-xl bg-[#F6F8FB]">
+      <div className="block min-w-full p-4 text-sm text-[#4F4F4F] border border-[#E0E0E0] rounded-lg bg-[#F6F8FB]">
         <p ref={urlRef} className='truncate w-[215px]'>{`${BASE_URL}${url}`}</p>
       </div>
-      <button className="text-white absolute right-1 bottom-1 bg-[#2F80ED] font-medium rounded-xl text-sm px-5 py-3.5" onClick={copyUrlToClipboard}>Copy Link</button>
+      <button className="text-white absolute right-2.5 bottom-2.5 bg-[#2F80ED] font-medium rounded-lg text-sm px-4 py-2" onClick={copyUrlToClipboard}>Copy Link</button>
     </div>
   )
 }
