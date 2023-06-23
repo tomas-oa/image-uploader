9f26fee0bfe04375e8b7531b9646dd2f43ec561b
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-06-22 20:29:36 -0400

    footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import Upload from './components/Upload'
+import Footer from './components/Footer'
 
 export default function Page() {
   return (
     <main className="min-h-screen min-w-screen grid place-content-center text-center">
       <Upload />
-      <footer className="absolute bottom-1 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs">
-        <small>created by <a className="text-[#09f]" href="https://github.com/tomas-oa">tomas-oa</a> - devChallenges.io</small>
-        <br />
-        <small> <span>&copy;</span> Copyright 2023 Jipi Corp S.A. </small>
-      </footer>
+      <Footer />
     </main>
   )
 }
