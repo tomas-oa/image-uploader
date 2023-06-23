67d2c005a2660b570ae1618498cede31f9cc648e
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-06-22 20:27:03 -0400

    footer fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Page() {
       <Upload />
       <footer className="absolute bottom-1 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs">
         <small>created by <a className="text-[#09f]" href="https://github.com/tomas-oa">tomas-oa</a> - devChallenges.io</small>
+        <br />
         <small> <span>&copy;</span> Copyright 2023 Jipi Corp S.A. </small>
       </footer>
     </main>
