6844c37cecc5a57b567564fcd747e731f0712c3d
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-09-28 02:46:46 -0300

    feat: :lipstick: footer shows always

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer () {
   return (
-    <footer className="hidden sm:block md:block lg:block absolute -bottom-2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs text-center">
+    <footer className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs text-center">
       <small>created by <a className="text-[#09f]" href="https://github.com/tomas-oa">tomas-oa</a> - devChallenges.io</small>
       <br />
       <small> <span>&copy;</span> Copyright 2023 Jipi Corp S.A. </small>
