b9194fd08944cf3f05ae9ffe1b619d2c65454929
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-10-13 18:59:49 -0300

    fix: :lipstick: footer postion
    moved to be visible on mobile

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,7 @@
 export default function Footer () {
   return (
-    <footer className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs text-center">
-      <small>created by <a className="text-[#09f]" href="https://github.com/tomas-oa">tomas-oa</a> - devChallenges.io</small>
-      <br />
-      <small> <span>&copy;</span> Copyright 2023 Jipi Corp S.A. </small>
+    <footer className="absolute top-6 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs text-center">
+      <small className='text-xs'>made with<span className='mx-1 text-red-500'>&hearts;</span>by <a className="text-[#09f]" href="https://github.com/tomas-oa">tomas-oa</a></small>
     </footer>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body className={`${poppins.className} relative min-h-[dvh]`}>
         {children}
         <Footer />
       </body>
